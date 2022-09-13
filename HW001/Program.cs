f407de66a1d2e7526a1bc03a44c9649719605f08
Valentin <dirk_31@mail.ru>
2022-09-13 16:12:55 +0700

    Hw

---
+++ HW001/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - да.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - Нет.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
